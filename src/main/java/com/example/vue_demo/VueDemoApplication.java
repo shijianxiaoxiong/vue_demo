04e60de8829fb0ef75115e7fb25961a4c22d538e
shijianxiaoxiong <782599753@qq.com>
2020-07-05 22:14:52 +0800

    first commit

---
+++ src/main/java/com/example/vue_demo/VueDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.vue_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueDemoApplication.class, args);
+    }
+
+}
